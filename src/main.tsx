b1c4f48be9f808de34b9db896ae50e19ca84672d
Daedu86 <50363036+Daedu86@users.noreply.github.com>
2026-06-03 22:26:34 +0200

    Wire E2E training performance harness

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { E2ETrainingPerfHarness, configureE2ETrainingPerf } from './components/training/E2ETrainingPerfHarness.tsx'
+
+const params = new URLSearchParams(window.location.search)
+const useE2ETrainingHarness = import.meta.env.DEV && params.get('e2eTraining') === '1'
+
+if (useE2ETrainingHarness) {
+  configureE2ETrainingPerf()
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {useE2ETrainingHarness ? <E2ETrainingPerfHarness /> : <App />}
   </StrictMode>,
 )
 
