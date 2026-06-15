ca254af4f35ec21e7ddc5793c59d2d3b5b889f0a
Daedu86 <50363036+Daedu86@users.noreply.github.com>
2026-06-15 16:08:57 +0200

    Extract Dicta access runtime boundary

---
+++ src/app/useDictaAccessRuntime.ts
@@ -0,0 +1,48 @@
+import { useAdminFileInventory } from './useAdminFileInventory';
+import { useAdminProfileAccessActions } from './useAdminProfileAccessActions';
+import { useAuthProfileRuntime } from './useAuthProfileRuntime';
+import { useDictaSupabaseRuntime } from './useDictaSupabaseRuntime';
+import { useOpenRouterModelRuntime } from './useOpenRouterModelRuntime';
+
+type UseDictaAccessRuntimeOptions = {
+  workspaceMode: string;
+  localDevFeaturesAvailable: boolean;
+};
+
+export function useDictaAccessRuntime({
+  workspaceMode,
+  localDevFeaturesAvailable,
+}: UseDictaAccessRuntimeOptions) {
+  const {
+    syncConfig,
+    supabaseClient,
+  } = useDictaSupabaseRuntime();
+  const authProfileRuntime = useAuthProfileRuntime({
+    syncConfig,
+    supabaseClient,
+  });
+  const openRouterModelRuntime = useOpenRouterModelRuntime({
+    syncConfig,
+    appProfile: authProfileRuntime.appProfile,
+    getAuthHeaders: authProfileRuntime.getAuthHeaders,
+  });
+  const adminFileInventoryRuntime = useAdminFileInventory({
+    workspaceMode,
+    localDevFeaturesAvailable,
+  });
+  const adminProfileAccessRuntime = useAdminProfileAccessActions({
+    getAuthHeaders: authProfileRuntime.getAuthHeaders,
+    appProfile: authProfileRuntime.appProfile,
+    setAppProfile: authProfileRuntime.setAppProfile,
+    setVisibleProfiles: authProfileRuntime.setVisibleProfiles,
+  });
+
+  return {
+    syncConfig,
+    supabaseClient,
+    ...authProfileRuntime,
+    ...openRouterModelRuntime,
+    ...adminFileInventoryRuntime,
+    ...adminProfileAccessRuntime,
+  };
+}
