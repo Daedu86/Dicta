53e802604900f0cc8791f7918ebe284fb55852c3
Daedu86 <50363036+Daedu86@users.noreply.github.com>
2026-06-16 23:01:06 +0200

    Modularize focused training runtime delegate types

--- src/app/focusedTrainingRuntimeDelegateArgs.ts
+++ src/app/focusedTrainingRuntimeDelegateArgs.ts
@@ -1,61 +1,12 @@
-import { useActiveSessionStateSync } from './useActiveSessionStateSync';
-import { useFocusedTrainingLiveMetrics } from './useFocusedTrainingLiveMetrics';
-import { useFocusedTrainingRouteRuntime } from './useFocusedTrainingRouteRuntime';
-import { useTtsPlaybackIntervalsRuntime } from './useTtsPlaybackIntervalsRuntime';
-import { useTtsSessionOrchestrationRuntime } from './useTtsSessionOrchestrationRuntime';
+import type {
+  FocusedTrainingRuntimeDelegateArgs,
+  UseFocusedTrainingRuntimeArgs,
+} from './focusedTrainingRuntimeDelegateTypes';
 
-type LiveMetricsArgs = Parameters<typeof useFocusedTrainingLiveMetrics>[0];
-type IntervalsArgs = Parameters<typeof useTtsPlaybackIntervalsRuntime>[0];
-type StateSyncArgs = Parameters<typeof useActiveSessionStateSync>[0];
-type TtsOrchestrationArgs = Parameters<typeof useTtsSessionOrchestrationRuntime>[0];
-type FocusedTrainingRouteArgs = Parameters<typeof useFocusedTrainingRouteRuntime>[0];
-
-type PlaybackIntervalsDelegateArgs = Omit<IntervalsArgs, 'ttsHasText' | 'tickMs'>;
-type ActiveSessionSyncDelegateArgs = Omit<
-  StateSyncArgs,
-  'activeVisibleAccuracy' | 'activeVisibleScore' | 'activePoints'
->;
-type TtsOrchestrationDelegateArgs = Omit<TtsOrchestrationArgs, 'ttsHasText' | 'ttsTranscript'>;
-type FocusedTrainingRouteDelegateArgs = Omit<
-  FocusedTrainingRouteArgs,
-  | 'activeVisibleScore'
-  | 'activeLiveScoreHelpText'
-  | 'activeLivePointsLabel'
-  | 'activeLivePointsHelpText'
-  | 'activeVisibleAccuracy'
-  | 'activeLiveAccuracyHelpText'
-  | 'ttsHasText'
-  | 'ttsTranscript'
-  | 'estimateTtsSpokenWordIndex'
-  | 'seekTtsPlayback'
-  | 'resetSession'
-  | 'playTts'
-  | 'resumeTts'
-  | 'pauseTts'
-  | 'stopTtsPlayback'
-  | 'onTtsPracticeChange'
-  | 'onTtsPracticeKeyDown'
-  | 'submitTtsSession'
->;
-
-export type UseFocusedTrainingRuntimeArgs =
-  LiveMetricsArgs &
-  PlaybackIntervalsDelegateArgs &
-  ActiveSessionSyncDelegateArgs &
-  TtsOrchestrationDelegateArgs &
-  FocusedTrainingRouteDelegateArgs & {
-    config: {
-      tickMs: number;
-    };
-  };
-
-export type FocusedTrainingRuntimeDelegateArgs = {
-  liveMetrics: LiveMetricsArgs;
-  playbackIntervals: PlaybackIntervalsDelegateArgs;
-  activeSessionSync: ActiveSessionSyncDelegateArgs;
-  ttsOrchestration: TtsOrchestrationDelegateArgs;
-  focusedRoute: FocusedTrainingRouteDelegateArgs;
-};
+export type {
+  FocusedTrainingRuntimeDelegateArgs,
+  UseFocusedTrainingRuntimeArgs,
+} from './focusedTrainingRuntimeDelegateTypes';
 
 export function buildFocusedTrainingRuntimeDelegateArgs(
   args: UseFocusedTrainingRuntimeArgs,
