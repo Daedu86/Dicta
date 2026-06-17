91abec5521e59be24ab5a79b83f16ff49db092aa
Daedu86 <50363036+Daedu86@users.noreply.github.com>
2026-06-17 17:07:42 +0200

    Extract INP attribution constants

---
+++ src/observability/inpAttribution/constants.ts
@@ -0,0 +1,22 @@
+export const MIN_TRACKED_INP = 200;
+export const EVENT_DURATION_THRESHOLD = 40;
+export const MAX_SELECTOR_LENGTH = 180;
+
+export const REPORTABLE_EVENTS = new Set([
+  'auxclick',
+  'beforeinput',
+  'click',
+  'compositionend',
+  'contextmenu',
+  'dblclick',
+  'input',
+  'keydown',
+  'keypress',
+  'keyup',
+  'mousedown',
+  'mouseup',
+  'pointerdown',
+  'pointerup',
+  'touchend',
+  'touchstart',
+]);
