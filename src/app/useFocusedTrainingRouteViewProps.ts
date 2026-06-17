4a4faf8a0bcc95918c91f337426ee2f4d04694e0
Daedu86 <50363036+Daedu86@users.noreply.github.com>
2026-06-17 11:16:57 +0200

    Extract focused training route view props hook

---
+++ src/app/useFocusedTrainingRouteViewProps.ts
@@ -0,0 +1,61 @@
+import { useFocusedTrainingViewProps } from './useFocusedTrainingViewProps';
+import type { useFocusedTrainingInputTelemetryRuntime } from './useFocusedTrainingInputTelemetryRuntime';
+import type { useFocusedTrainingRouteControls } from './useFocusedTrainingRouteControls';
+import type { useFocusedTrainingRouteGenerationButtons } from './useFocusedTrainingRouteGenerationButtons';
+import type { useFocusedTrainingRoutePresentationState } from './useFocusedTrainingRoutePresentationState';
+import type { UseFocusedTrainingRouteRuntimeArgs } from './useFocusedTrainingRouteRuntimeTypes';
+
+interface UseFocusedTrainingRouteViewPropsArgs {
+  args: UseFocusedTrainingRouteRuntimeArgs;
+  focusedTrainingControls: ReturnType<typeof useFocusedTrainingRouteControls>;
+  focusedImmediateInputHandler: ReturnType<typeof useFocusedTrainingInputTelemetryRuntime>;
+  focusedTrainingGenerationButtons: ReturnType<typeof useFocusedTrainingRouteGenerationButtons>;
+  focusedTrainingPresentationState: ReturnType<typeof useFocusedTrainingRoutePresentationState>;
+  replayFocusedTts: () => void;
+}
+
+export function useFocusedTrainingRouteViewProps({
+  args,
+  focusedTrainingControls,
+  focusedImmediateInputHandler,
+  focusedTrainingGenerationButtons,
+  focusedTrainingPresentationState,
+  replayFocusedTts,
+}: UseFocusedTrainingRouteViewPropsArgs) {
+  return useFocusedTrainingViewProps({
+    activeSession: args.activeSession,
+    submissionMeta: args.activeTrainingSubmissionMeta,
+    activeInputLabel: args.activeInputLabel,
+    sessionStatus: args.sessionStatus,
+    sourceLabel: focusedTrainingPresentationState.focusedSourceLabel,
+    progressLabel: focusedTrainingPresentationState.focusedProgressLabel,
+    statusLabel: args.ttsStatus,
+    currentTextValue: focusedTrainingPresentationState.focusedTextValue,
+    onTextChange: args.onTtsPracticeChange,
+    onImmediateTextChange: focusedImmediateInputHandler,
+    onTextKeyDown: args.onTtsPracticeKeyDown,
+    textPlaceholder: focusedTrainingPresentationState.focusedTextPlaceholder,
+    activeVisibleScore: args.activeVisibleScore,
+    liveScoreHelpText: args.activeLiveScoreHelpText,
+    livePointsLabel: args.activeLivePointsLabel,
+    livePointsHelpText: args.activeLivePointsHelpText,
+    activeVisibleAccuracy: args.activeVisibleAccuracy,
+    liveAccuracyHelpText: args.activeLiveAccuracyHelpText,
+    lagSec: args.lagSec,
+    activeSessionFinished: args.activeSessionFinished,
+    focusedTrainingControls,
+    ttsHasText: args.ttsHasText,
+    ttsPlayerDurationSec: focusedTrainingPresentationState.ttsPlayerDurationSec,
+    onReplayFocusedTts: replayFocusedTts,
+    message: focusedTrainingPresentationState.focusedTrainingMessage,
+    messageTone: focusedTrainingPresentationState.focusedTrainingMessageTone,
+    pendingSessions: args.pendingSessions,
+    activeSessionId: args.activeSessionId,
+    onOpenPendingSession: args.openWorkspaceForSession,
+    onDeletePendingSession: args.deleteSession,
+    syncStatus: args.supabaseSyncStatus,
+    pendingSyncSummary: args.pendingSyncSummary,
+    isOnline: args.isOnline,
+    generationButtons: focusedTrainingGenerationButtons,
+  });
+}
