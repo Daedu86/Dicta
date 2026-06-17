fe5fdf9f3e27aff4e55d8a7db8d9f8ec3acb6117
Daedu86 <50363036+Daedu86@users.noreply.github.com>
2026-06-17 18:19:07 +0200

    refactor(tts): add orchestration delegate arg section barrel

---
+++ src/app/ttsSessionOrchestrationDelegateArgSections.ts
@@ -0,0 +1,7 @@
+export { buildTtsSessionBrowserPlaybackArgs } from './ttsSessionOrchestrationDelegateArgSections/browserPlaybackArgs';
+export { buildTtsSessionKeyboardRemapArgs } from './ttsSessionOrchestrationDelegateArgSections/keyboardRemapArgs';
+export { buildTtsSessionPlaybackControlsArgs } from './ttsSessionOrchestrationDelegateArgSections/playbackControlsArgs';
+export { buildTtsSessionPlaybackMetricsArgs } from './ttsSessionOrchestrationDelegateArgSections/playbackMetricsArgs';
+export { buildTtsSessionPracticeInputArgs } from './ttsSessionOrchestrationDelegateArgSections/practiceInputArgs';
+export { buildTtsSessionResetSessionArgs } from './ttsSessionOrchestrationDelegateArgSections/resetSessionArgs';
+export { buildTtsSessionSubmitSessionArgs } from './ttsSessionOrchestrationDelegateArgSections/submitSessionArgs';
