dfb4028143f8bd3b1152ef0442e960e0c13efd63
Daedu86 <50363036+Daedu86@users.noreply.github.com>
2026-06-20 15:53:04 +0200

    Use single direct generation busy state

--- src/app/useOpenRouterGenerationBusyState.ts
+++ src/app/useOpenRouterGenerationBusyState.ts
@@ -2,15 +2,9 @@ import { useState } from 'react';
 
 export function useOpenRouterGenerationBusyState() {
   const [directOpenRouterBusy, setDirectOpenRouterBusy] = useState(false);
-  const [directIntermediateOpenRouterBusy, setDirectIntermediateOpenRouterBusy] = useState(false);
-  const [directAdvancedOpenRouterBusy, setDirectAdvancedOpenRouterBusy] = useState(false);
 
   return {
     directOpenRouterBusy,
     setDirectOpenRouterBusy,
-    directIntermediateOpenRouterBusy,
-    setDirectIntermediateOpenRouterBusy,
-    directAdvancedOpenRouterBusy,
-    setDirectAdvancedOpenRouterBusy,
   };
 }
